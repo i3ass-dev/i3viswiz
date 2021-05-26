0fe9359984f37fb7409e5551dd746b12b3f842db
budRich <robstenklippa@gmail.com>
2021-05-26 12:12:45 +0200

    [CLEAN] accurate comment in messy()

--- lib/messy.sh
+++ lib/messy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 messy() {
-
   # arguments are valid i3-msg arguments
-  # separate resize commands and execute
-  # all commands at once in cleanup()
+  # execute all commands at once in cleanup()
   (( __o[verbose] )) && ERM "m $*"
   (( __o[dryrun]  )) || _msgstring+="$*;"
 }
